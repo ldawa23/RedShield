52c01a2bfa5befa0af3dfa838eaeb090820bd227
Dawa Lama <ldawa23@tbc.edu.np>
2025-12-07 12:29:32 -0500

    Updated features

--- models/scan.py
+++ models/scan.py
@@ -6,18 +6,18 @@ import uuid
 
 @dataclass
 class Scan:
-    id: str                 
+    id: str
     target: str
     started_at: datetime
-    status: str             # eg: Running, COmpleted, Failed
+    status: str
     vulnerabilities: List[Vulnerability] = field(default_factory=list)
 
     @staticmethod
     def new(target: str) -> "Scan":
-        """Scan with default values for practise"""
         return Scan(
-                id=str(uuid.uuid4()),
-                target=target,
-                started_at=datetime.now(),
-                status="Running",
+            id=str(uuid.uuid4()),
+            target=target,
+            started_at=datetime.now(),
+            status="Running",
         )
+
